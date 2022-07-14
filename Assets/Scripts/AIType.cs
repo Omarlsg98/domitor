6601a0d4a3a14e099408132b1e9c9809dce83473
Omar Sanchez <omarlsg98@gmail.com>
2022-07-13 23:21:15 -0500

    Add Stalker AI

--- Assets/Scripts/AIType.cs
+++ Assets/Scripts/AIType.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using static Demon;
 
 public enum AIType
@@ -9,15 +10,64 @@ public enum AIType
 
 public abstract class AIInstance{
     public Demon demon;
+    public Demon player;
 
-    public AIInstance(Demon demon){
+    protected int prevMovementResult;
+
+    public AIInstance(Demon demon, Demon player){
         this.demon = demon;
+        this.player = player;
+        prevMovementResult = -1;
+    }
+
+    protected abstract (int horizontalAxis, int verticalAxis) getAxis();
+    protected abstract AttackButton getAttackButton();
+
+    public void Movement()
+    {
+        (int horizontalAxis, int verticalAxis)  = getAxis();
+        prevMovementResult = demon.updatePosition(horizontalAxis, verticalAxis);
+    }
+
+    public void Attack()
+    {
+        AttackButton attackBtn = getAttackButton();
+        if (attackBtn != AttackButton.None) { 
+            demon.attack(attackBtn);
+        }
+    }
+
+    protected int getVerticallDiff(){
+        return player.actPosition.y - demon.actPosition.y;
     }
 }
 
-public class AIPersecutor : AIInstance
+public class AIStalker : AIInstance
 {
-    public AIPersecutor(Demon demon) : base(demon)    
-    {
+    public AIStalker(Demon demon, Demon player) : base(demon, player) {}
+
+    protected override (int horizontalAxis, int verticalAxis) getAxis(){
+        (int horizontalAxis, int verticalAxis) = (0, 0);
+        int verticalDiff = getVerticallDiff();
+        if(verticalDiff == 0){
+            verticalAxis = 0;
+        }else if (verticalDiff > 0){
+            verticalAxis = 1;
+        }else {
+            verticalAxis = -1;
+        }
+
+        if(prevMovementResult == 0){
+            horizontalAxis = Random.Range(-1, 2);
+        }
+        
+        return (horizontalAxis, verticalAxis);
+    }
+
+    protected override AttackButton getAttackButton(){
+        if(getVerticallDiff() == 0){
+            return AttackButton.Attack1;
+        }
+        return AttackButton.None;
     }
 }
