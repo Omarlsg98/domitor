a2eb4d0e2d33d853b949e82d27a3fa109fd5bb34
Omar Sanchez <omarlsg98@gmail.com>
2022-07-14 17:25:42 -0500

    Add sound delay

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,22 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class DemonSoundController
+public class MyAudioClip{
+    public AudioClip audio;
+    public int secondsToWait;
+}
+
+public abstract class SoundController
 {
-    private AudioSource audioSource;
-    public AudioClip damageAudio;
-    public AudioClip movementAudio;
-    public AudioClip attackAudio;
+    protected AudioSource audioSource;
+    private Main mainController;
 
     public void setAudioSource(AudioSource audioSource){
         this.audioSource = audioSource;
+        mainController = GameObject.FindWithTag("GameController").GetComponent<Main>();
+    }
+    
+    protected void reproduceSound(MyAudioClip clip){
+        mainController.StartCoroutine(_reproduceSound(clip));
     }
 
+    private IEnumerator _reproduceSound(MyAudioClip clip){
+        yield return new WaitForSeconds(clip.secondsToWait);
+        audioSource.PlayOneShot(clip.audio);
+    }
+}
+
+[System.Serializable]
+public class DemonSoundController : SoundController
+{
+    public MyAudioClip damageAudio;
+    public MyAudioClip movementAudio;
+    public MyAudioClip attackAudio;
+
+   
     public void reproduceAttack(){
         reproduceSound(attackAudio);
     }
 
-    public void reproduceAttack(AudioClip attackAudio){
+    public void reproduceAttack(MyAudioClip attackAudio){
         if (attackAudio == null){
             reproduceSound(this.attackAudio);
         }else {
@@ -30,9 +54,5 @@ public class DemonSoundController
 
     public void reproduceDamage(){
         reproduceSound(damageAudio);
-    }
-
-    private void reproduceSound(AudioClip audio){
-        audioSource.PlayOneShot(audio);
     }
 }
